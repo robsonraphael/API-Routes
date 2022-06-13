2b9db1d9362287838e5779a7a6cfd692d57e77d1
Robson Raphael <79436128+robsonraphael@users.noreply.github.com>
2022-06-13 14:48:35 -0300

    Fix index.js
    fix response Error

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ http.createServer((request, response) => {
   if (request.url === '/') {
     fs.readFile('index.html', (error, data) => {
       if (error) {
-        response.writeHead(400, { "Content-Type": "text/plain" });
-        response.write(error);
-        console.log(error);
-        return response.end();
+        console.log(error)
       }
       response.writeHead(200, { "Content-Type": "text/html" });
       response.write(data);
@@ -19,10 +16,7 @@ http.createServer((request, response) => {
   if (request.url === '/about') {
     fs.readFile('about.html', (error, data) => {
       if (error) {
-        response.writeHead(400, { "Content-Type": "text/plain" });
-        response.write(error);
-        console.log(error);
-        return response.end();
+        console.log(error)
       }
       response.writeHead(200, { "Content-Type": "text/html" });
       response.write(data);
@@ -32,11 +26,9 @@ http.createServer((request, response) => {
   if (request.url === '/contact') {
     fs.readFile('contact.html', (error, data) => {
       if (error) {
-        response.writeHead(400, { "Content-Type": "text/plain" });
-        response.write(error);
-        console.log(error);
-        return response.end();
+        console.log(error)
       }
+
       response.writeHead(200, { "Content-Type": "text/html" });
       response.write(data);
       return response.end();
@@ -44,6 +36,9 @@ http.createServer((request, response) => {
   }
   if (request.url !== true) {
     fs.readFile('404.html', (error, data) => {
+      if (error) {
+        console.log(error)
+      }
       response.writeHead(404, { "Content-Type": "text/html" });
       response.write(data);
       response.end();
